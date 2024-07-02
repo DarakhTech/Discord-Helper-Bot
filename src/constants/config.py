91c76015c742007c537810c2c9d8dfb2c456c299
DarakhTech <darakhtech@gmail.com>
2024-07-02 17:22:12 +0530

    updated yml

--- src/constants/config.py
+++ src/constants/config.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file if present
+# Load environment variables from .env file
 load_dotenv()
 
 # Constants like bot token and Google Sheets settings
@@ -9,3 +9,9 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 SHEET_NAME = os.getenv('SHEET_NAME')
 WORKSHEET_NAME = os.getenv('WORKSHEET_NAME')
 GOOGLE_SHEETS_CREDENTIALS_PATH = 'src/constants/token.json'
+
+# Add logging to check if the environment variables are loaded correctly
+print(f"BOT_TOKEN: {BOT_TOKEN}")
+print(f"SHEET_NAME: {SHEET_NAME}")
+print(f"WORKSHEET_NAME: {WORKSHEET_NAME}")
+print(f"Google Sheets Credentials Path: {GOOGLE_SHEETS_CREDENTIALS_PATH}")
