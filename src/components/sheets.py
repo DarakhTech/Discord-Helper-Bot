533ee5433b039371fdd533e87b834194429d6299
DarakhTech <darakhtech@gmail.com>
2024-07-23 13:44:55 +0530

    update/cosmetic_3

--- src/components/sheets.py
+++ src/components/sheets.py
@@ -40,7 +40,7 @@ async def update_sheet(ctx, channel_id, message, bot):
         worksheet.append_row(row)
         await ctx.channel.purge(limit=2)
         await channel.send(
-            f"""## {data["Name of Company"]}```Title:{data["Job Title"]}\nID:{data["Job ID"]}```{data["Link"]}"""
+            f"""## {data["Name of Company"]} ```Title:{data["Job Title"]}\nID:{data["Job ID"]}```{data["Link"]}"""
         )
         
     except json.JSONDecodeError:
